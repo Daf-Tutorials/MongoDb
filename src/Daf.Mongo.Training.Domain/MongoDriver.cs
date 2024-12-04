73d038314985134acfb0b97e4398621b741e081d
Daf <afahd.ali@gmail.com>
2024-12-04 09:32:34 +0100

    feat: turn indent size to 2 spaces and modify .editorconfig accordingly

--- src/Daf.Mongo.Training.Domain/MongoDriver.cs
+++ src/Daf.Mongo.Training.Domain/MongoDriver.cs
@@ -1,12 +1,14 @@
 ﻿using MongoDB.Driver;
+
 namespace Daf.Mongo.Training.Domain;
 
 public class MongoDriver
 {
-    private const string ConnectionString =
-        "mongodb+srv://dafdev:jpHtRY3vM6bvNK7O@normal.aycpm.mongodb.net/?retryWrites=true&w=majority&appName=Normal";
-    private readonly MongoClient _client = new(ConnectionString);
+  private const string ConnectionString =
+    "mongodb+srv://dafdev:jpHtRY3vM6bvNK7O@normal.aycpm.mongodb.net/?retryWrites=true&w=majority&appName=Normal";
 
-    public IEnumerable<MongoDB.Bson.BsonDocument> GetDatabases() 
-        => _client.ListDatabases().ToList();
+  private readonly MongoClient _client = new(ConnectionString);
+
+  public IEnumerable<MongoDB.Bson.BsonDocument> GetDatabases()
+    => _client.ListDatabases().ToList();
 }
